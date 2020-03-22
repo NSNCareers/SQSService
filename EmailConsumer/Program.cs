91f56ab4d2fe34fd405ef5f5dcd704a40425f5a0
Ajang Ngwesse <elvisdemoprojects@outlook.com>
2020-03-22 15:46:23 +0000

    First commit

---
+++ EmailConsumer/Program.cs
@@ -0,0 +1,63 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using System.Configuration;
+using System.Linq;
+
+namespace EmailConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var accessKey = ConfigurationManager.AppSettings.Get("AccessKey");
+            var secretKey = ConfigurationManager.AppSettings.Get("SecretKey");
+            var queueName = "EmailQueue2";
+
+            AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+            IAmazonSQS amazonSQS = new AmazonSQSClient(credentials, RegionEndpoint.EUWest2);
+
+            var queueUrl = amazonSQS.GetQueueUrlAsync(queueName).Result.QueueUrl;
+
+            var receveMessagerequest = new ReceiveMessageRequest()
+            {
+                QueueUrl = queueUrl
+            };
+
+            var receiveMessageResponse = amazonSQS.ReceiveMessageAsync(receveMessagerequest).Result;
+
+            foreach (var message in receiveMessageResponse.Messages)
+            {
+                Console.WriteLine("Message \n");
+                Console.WriteLine($" MessageId: {message.MessageId} \n");
+                Console.WriteLine($" Receipthandle: {message.ReceiptHandle} \n");
+                Console.WriteLine($" MSD5Body: {message.MD5OfBody} \n");
+                Console.WriteLine($" Body: {message.Body} \n");
+
+                foreach (var attributes in message.Attributes)
+                {
+                    Console.WriteLine("Attributes \n");
+                    Console.WriteLine($" Name: {attributes.Key} \n");
+                    Console.WriteLine($" Value: {attributes.Value} \n");
+                }
+
+                var messageHandle = receiveMessageResponse.Messages.FirstOrDefault()?.ReceiptHandle;
+
+                var deleteRequest = new DeleteMessageRequest
+                {
+                    QueueUrl = queueUrl,
+                    ReceiptHandle = messageHandle
+                };
+
+                Console.WriteLine($"deleting message with MessageHandle: {messageHandle}");
+
+                amazonSQS.DeleteMessageAsync(deleteRequest);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ EmailProducer/Program.cs
@@ -0,0 +1,67 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using System.Configuration;
+
+namespace EmailProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+            Console.WriteLine("*************************");
+            Console.WriteLine("Amazon SQS");
+            Console.WriteLine("************************\n");
+
+            //var accessKey = JsonConfig.GetJsonValue("AccessKey");
+            //var secretKey = JsonConfig.GetJsonValue("SecretKey");
+            var accessKey = ConfigurationManager.AppSettings.Get("AccessKey");
+            var secretKey = ConfigurationManager.AppSettings.Get("SecretKey");
+
+            var queueName = "EmailQueue2";
+
+            AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+            IAmazonSQS amazonSQS = new AmazonSQSClient(credentials,RegionEndpoint.EUWest2);
+
+            Console.WriteLine("Create a queue called EmailQueue.\n");
+            logger.Info("Create a queue called EmailQueue");
+            logger.Info("Testing Logs");
+
+            var sqsRequest = new CreateQueueRequest
+            {
+                QueueName = queueName
+            };
+
+            var createQueueResponse = amazonSQS.CreateQueueAsync(sqsRequest).Result;
+
+            var myQueueRrl = createQueueResponse.QueueUrl;
+
+            var listQueueRequest = new ListQueuesRequest();
+            var listQueuesResponce = amazonSQS.ListQueuesAsync(listQueueRequest);
+
+            Console.WriteLine("List of Amazon SQS queuse.\n");
+
+            foreach (var queueurl in listQueuesResponce.Result.QueueUrls)
+            {
+                Console.WriteLine($"QueueUrl : {queueurl}");
+            }
+
+            Console.WriteLine("Sendin a message to our emailQueue. \n");
+
+            var sqsMessageRequest = new SendMessageRequest
+            {
+                QueueUrl = myQueueRrl,
+                MessageBody = "Email Information"
+            };
+
+            amazonSQS.SendMessageAsync(sqsMessageRequest);
+
+            Console.WriteLine("Finished sending message to our sqs queue. \n");
+            Console.ReadLine();
+        }
+    }
+}
